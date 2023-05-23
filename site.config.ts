280dedd74d22105379878999e8311a4725975698
Wei <kiwi072400.lin@gmail.com>
2023-05-23 11:16:52 +0800

    Update site config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '24522e2cd8384ba3a469087589ed4c24',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '36b95c8e-57cf-42ed-b1c5-cadc4ee00253',
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '林奇葦',
+  domain: '24.7wei.xyz',
+  author: '林奇葦',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '哈囉！我是林奇葦，目前就讀於國立清華大學。除了學生的身分之外，我也是一名獨立接案工作者，擅長前端網頁開發、SEO與總體行           銷規劃。希望從現在開始累積不同領域的經驗，想成為更有溫度的人(๑´ڡ`๑)',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'l7wei',
+  linkedin: '7wei',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
